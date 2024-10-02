d6678e6e3ba58dd61f0db10fca38302fa1d64352
Yoana <yoanabarbie@icloud.com>
2024-10-02 16:47:48 +0200

    feat: routes configured for the moment

--- src/views/Body/Body.tsx
+++ src/views/Body/Body.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Error } from '../Error/Error'
+import { Home } from '../Home/Home'
+import { Register } from '../Register/Register'
+import { Login } from '../Login/Login'
+import { New } from '../Bookings/New/New.tsx'
+import { Current } from '../Bookings/Current/Current.tsx'
+import { Reports } from '../Reports/Reports.tsx'
+import { Users } from '../Users/Users.tsx'
 
 export const Body = () => {
   return (
     <>
     <Routes>
         <Route path="*" element={<Error />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/bookings" element={<Current />} />
+        <Route path="/bookings/new" element={<New />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/users" element={<Users />} />
     </Routes>
     </>
   )
--- src/views/Bookings/newBooking/newBooking.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const newBooking = () => {
-  return (
-    <div>newBooking</div>
-  )
-}
