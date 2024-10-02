bb2176555d990968255ad983e1836e1aa982251c
Yoana <yoanabarbie@icloud.com>
2024-10-02 17:43:37 +0200

    feat: added MUI as library choice and Navbar component completed

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Navbar } from '../../components/Navbar/Navbar'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Navbar />
+    </>
   )
 }
